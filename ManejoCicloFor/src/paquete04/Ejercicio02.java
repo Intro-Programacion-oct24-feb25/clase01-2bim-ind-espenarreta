e8d95b38f8a533f96aaa011560096efa38c6826a
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-12-05 12:29:43 -0500

    Cambios

--- ManejoCicloFor/src/paquete04/Ejercicio02.java
+++ ManejoCicloFor/src/paquete04/Ejercicio02.java
@@ -9,8 +9,19 @@ package paquete04;
  * @author utpl
  */
 public class Ejercicio02 {
-        public static void main(String[] args) {
-            
-        
+
+    public static void main(String[] args) {
+        double celcius = 0;
+        double fahrenheit;
+        String cadena = "";
+        for (int operacion = 0; operacion <= 20; operacion++){
+            for (fahrenheit = 20.00; fahrenheit <= 100; fahrenheit+=4)
+                celcius = 5/9 * (fahrenheit - 32);
+                
+             cadena = String.format("%sOperación(%d)Fahrenheit: %.2f ===>"
+                     + " Celcius: %.2f\n", cadena, operacion,
+                     fahrenheit, celcius);
+        }
+        System.out.printf(cadena);
     }
 }
