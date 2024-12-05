594849dff15472caae9afc117384ba0016a0c537
Eduardo Peñarreta <espenarreta@utpl.edu.ec>
2024-12-05 11:35:10 -0500

    Cambios

--- ManejoCicloFor/src/paquete04/Ejercicio01.java
+++ ManejoCicloFor/src/paquete04/Ejercicio01.java
@@ -9,25 +9,14 @@ package paquete04;
  * @author utpl
  */
 public class Ejercicio01 {
-
+    
     public static void main(String[] args) {
         String cadena = "";
-        String cadenafinal = "";
-        for (int serie = 1; serie <= 4; serie++) {
+        
             for (int i = 30; i <= 10; i--) {
                 cadena = String.format("%s%d", cadena, i);
-            }
-            for (int i = 60; i <= 20; i -= 2) {
-                cadena = String.format("%s%d", cadena, i);
-            }
-            for (int i = 90; i <= 30; i -= 3) {
-                cadena = String.format("%s%d", cadena, i);
-            }
-            for (int i = 120; i <= 40; i -= 4) {
-                cadena = String.format("%s%d", cadena, i);
-            }
-            cadenafinal = String.format("%s", cadenafinal);
-        }
+                
 
+        }
     }
 }
